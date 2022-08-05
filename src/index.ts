778d7f2873cffa3350efb1cf4e6f33a2330a182f
Justin <xhergz@gmail.com>
2022-08-04 22:24:01 -0400

    Turned connection into an object

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,49 @@
 import mysql from "mysql2/promise";
 
+import DbConnection from "./connection";
 import Table from "./table";
 
 /**
  * The root Musqrat class that handles the MySQL connection and initializing connected components.
  */
 class Musqrat {
-    private _pool: mysql.Pool | null;
+  private _connection: DbConnection;
+  constructor() {
+    this._connection = new DbConnection();
+  }
 
-    constructor() {
-        this._pool = null;
-    }
+  /**
+   * Determines if the database is currently connected or not.
+   */
+  public get connected(): boolean {
+    return this._connection.connected;
+  }
 
-    /**
-     * Determines if the database is currently connected or not.
-     */
-    public get connected(): boolean {
-        return this._pool !== null;
-    }
+  /**
+   * Creates the connection to the database.
+   * @param config The MySQL pool connection options.
+   */
+  public connect(config: mysql.PoolOptions): void {
+    this._connection.connect(config);
+  }
 
-    /**
-     * Creates the connection to the database.
-     * @param config The MySQL pool connection options.
-     */
-    public connect(config: mysql.PoolOptions): void {
-        this._pool = mysql.createPool(config);
-    }
+  /**
+   * Disconnects from the database.
+   */
+  public async disconnect(): Promise<void> {
+    this._connection.disconnect();
+  }
 
-    /**
-     * Disconnects from the database.
-     */
-    public async disconnect(): Promise<void> {
-        if (this._pool !== null) {
-            await this._pool.end();
-            this._pool = null;
-        }
-    }
-
-    /**
-     * Initializes a table with the database connection.
-     * @param tableName The table name.
-     * @returns The initialized table object.
-     */
-    public initTable<TableSchema, PrimaryKey extends keyof TableSchema = never>(
-        tableName: string
-    ): Table<TableSchema, PrimaryKey> {
-        return new Table<TableSchema, PrimaryKey>(
-            tableName,
-            this._pool || undefined
-        );
-    }
+  /**
+   * Initializes a table with the database connection.
+   * @param tableName The table name.
+   * @returns The initialized table object.
+   */
+  public initTable<TableSchema, PrimaryKey extends keyof TableSchema = never>(
+    tableName: string
+  ): Table<TableSchema, PrimaryKey> {
+    return new Table<TableSchema, PrimaryKey>(tableName, this._connection);
+  }
 }
 
 export default new Musqrat();
